30f928ec75f7687d727b2d98a6bdbafdfd98caa1
Sandra <sandramtzd@hotmail.com>
2023-09-20 23:17:12 +0100

    added sidebar with errors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route} from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import AnotherSidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Contacts from "./scenes/contacts";
+// import Invoices from "./scenes/invoices";
+// import Bar from "./scenes/bar";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+
 
 function App() {
+  const [theme, colorMode] = useMode();
+
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <AnotherSidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element ={<Dashboard />} />
+              {/* <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
